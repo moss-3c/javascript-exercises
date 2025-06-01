99432f9da205ccb0ec5e99fba3c76a724b0b05ec
moss-3c <117598547+moss-cc@users.noreply.github.com>
2025-06-01 05:54:32 -0700

    Do exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    }
 
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
+
+    if (a > b) {
+        [a, b] = [b, a];
+    }
+
+    let sum = 0;
+    let numElements = b - a + 1;
+
+    // a + b / (numElements / 2)
+    // formula only works with even # of elements
+    // so for odd # of elements. pre-emptively add last element. 
+    // update numElements to not include last element
+    // and use formula on remaining elements
+
+    if (numElements % 2 === 1) {
+        sum += b;
+        numElements--;
+        b--;
+    }
+
+    sum += (a + b) * (numElements / 2);
+
+    return sum;
 };
+ sumAll(2,4);
 
 // Do not edit below this line
 module.exports = sumAll;
