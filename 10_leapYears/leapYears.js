a07873f9e3a5ef8049af8901f4d25a7873bac2fe
moss-3c <117598547+moss-cc@users.noreply.github.com>
2025-06-01 22:10:52 -0700

    Do exercises 10, 11

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+   
+    //div by 400 -> leap yr
+    //div by 100 -> not leap yr
+    //div by 4 -> leap year
 
+    let isLeapYear;
+
+    (year % 400 === 0) ? (isLeapYear = true) :
+        (year % 100 === 0) ? (isLeapYear = false) :
+        (year % 4 === 0) ? (isLeapYear = true) :
+        isLeapYear = false;
+
+
+    return isLeapYear;
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return +((5/9) * (f - 32)).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return +(c*(9/5) + 32).toFixed(1);
 };
 
 // Do not edit below this line
